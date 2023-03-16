712c995901c410ade57ae7c6c809d16ffe79b35c
minhnbnt <minhnbnt@gmail.com>
2023-03-16 08:59:25 +0700

    delete binary and update cpu.cc :)))

---
+++ .config/waybar/cpu2.cc
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+float cpu_clock() {
+	std::ifstream cpuinfo("/proc/cpuinfo");
+	std::string line;
+	float clock = 0;
+	int cores = 0;
+	while (std::getline(cpuinfo, line))
+		if (line.find("cpu MHz") != std::string::npos) {
+			clock += std::stof(line.substr(line.find(":") + 2));
+			cores++;
+			break;
+		};
+	return clock / cores / 1000;
+}
+
+float cpu_usage() {
+	long long a[10], prevtotal = 0, previdle;
+	std::ifstream cpuinfo("/proc/stat");
+	cpuinfo.ignore(256, ' ');
+	for (int i = 0; i < 10; i++)
+		cpuinfo >> a[i], prevtotal += a[i];
+	previdle = a[3] + a[4];
+	sleep(1), cpuinfo.clear(), cpuinfo.seekg(0);
+	cpuinfo.ignore(256, ' ');
+	long long total = 0, idle;
+	for (int i = 0; i < 10; i++)
+		cpuinfo >> a[i], total += a[i];
+	idle = a[3] + a[4];
+	cpuinfo.close();
+	long long totald = total - prevtotal, idled = idle - previdle;
+	return (totald - idled) * 100.0 / totald;
+}
+
+int main() {
+	std::cout << cpu_clock() << cpu_usage() << std::endl;
+	return 0;
+}
