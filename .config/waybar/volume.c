b2f2fb0be556d47829c42cbf9f01e4b09234d638
minhnbnt <minhnbnt@gmail.com>
2023-03-19 19:26:24 +0700

    update C modules

--- .config/waybar/volume.c
+++ .config/waybar/volume.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
-		FILE *mute = popen("pamixer --get-mute", "r");
-		char mute_status;
-		if (mute == NULL) {
-			printf("Error opening pipe!");
-			fclose(mute);
+		FILE *cmd = popen("pamixer --get-mute", "r");
+		if (cmd == NULL) {
+			printf("Error opening pipe!"), fclose(cmd);
 			return 1;
 		} else {
-			fgets(&mute_status, 2, mute);
-			fclose(mute);
-			if (mute_status == 't') {
-				printf("\n");
-			} else {
+			char mute_status;
+			fgets(&mute_status, 2, cmd);
+			if (mute_status == 't') printf("\n");
+			else {
 				int vol;
-				FILE *volume = popen("pamixer --get-volume", "r");
-				fscanf(volume, "%d", &vol);
-				fclose(volume);
-				if (vol == 0)
-					printf("");
-				else if (vol < 33)
-					printf("");
-				else if (vol < 66)
-					printf("");
-				else
-					printf("");
+				cmd = popen("pamixer --get-volume", "r");
+				fscanf(cmd, "%d", &vol), fclose(cmd);
+				if (!vol) printf("");
+				else if (vol < 33) printf("");
+				else if (vol < 66) printf("");
+				else printf("");
 				printf(" %d%%\n", vol);
 			}
 		}
 	} else if (argc == 2) {
-		if (strcmp(argv[1], "up") == 0)
-			system("pamixer -i 5");
-		else if (strcmp(argv[1], "down") == 0)
-			system("pamixer -d 5");
-		else if (strcmp(argv[1], "mute") == 0)
-			system("pamixer -t");
-	}
+		if (strcmp(argv[1], "up") == 0) system("pamixer -i 5");
+		else if (strcmp(argv[1], "down") == 0) system("pamixer -d 5");
+		else if (strcmp(argv[1], "mute") == 0) system("pamixer -t");
+	} else return 1;
 }
