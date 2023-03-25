122ef3c0afc4cdcbfd9e80b57a20232a44e5899e
minhnbnt <minhnbnt@gmail.com>
2023-03-25 20:57:26 +0700

    add loop function for cpu

--- .config/waybar/cpu.c
+++ .config/waybar/cpu.c
@@ -2,21 +2,24 @@
 #include <string.h>
 #include <unistd.h>
 
+#define ull unsigned long long
+
 char *power_profile(char *command) {
 	FILE *fp = popen(command, "r");
 	char buf[12];
-	fgets(buf, 100, fp), pclose(fp);
-	if (!strncmp(buf, "balanced", 8)) return "&#xf24e;";          // 
-	else if (!strncmp(buf, "performance", 11)) return "&#xf0e4;"; // 
-	else if (!strncmp(buf, "power-save", 10)) return "&#xf06c;";  // 
-	else return "&#xf2db;";                                       // 
+	fgets(buf, 12, fp), pclose(fp);
+	if (strncmp(buf, "balanced", 8) == 0) return "&#xf24e;";          // 
+	else if (strncmp(buf, "performance", 11) == 0) return "&#xf0e4;"; // 
+	else if (strncmp(buf, "power-save", 10) == 0) return "&#xf06c;";  // 
+	else return "&#xf2db;";                                           // 
 }
 
 float cpu_clock(char *path) {
 	FILE *fp = fopen(path, "r");
-	char buf[100], core = 0;
+	char buf[100];
+	int core = 0;
 	float cpu_clock, sum = 0;
-	while (fgets(buf, 100, fp))
+	while (fgets(buf, 100, fp) != NULL)
 		if (strncmp(buf, "cpu MHz", 7) == 0) {
 			sscanf(buf, "cpu MHz\t: %f", &cpu_clock);
 			core++, sum += cpu_clock;
@@ -25,22 +28,36 @@ float cpu_clock(char *path) {
 	return sum / core / 1000;
 }
 
-float cpu_usage(char *path) {
-	long long a[10];
+ull sumOfArray(ull *a, int n) {
+	ull sum = 0;
+	for (int i = 0; i < n; i++) sum += a[i];
+	return sum;
+}
+
+float cpu_usage(char *path, int interval) {
+	ull a[10];
 	FILE *fp = fopen(path, "r");
-	fscanf(fp, "%*s %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld", &a[0],
+	fscanf(fp, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
 	       &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
-	long long int previdle = a[3] + a[4], pretotal = 0;
-	for (int i = 0; i < 10; i++) pretotal += a[i];
-	sleep(1), fp = fopen(path, "r");
-	fscanf(fp, "%*s %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld", &a[0],
+	ull idle = a[3] + a[4], total = sumOfArray(a, 10);
+	sleep(interval), fp = fopen(path, "r");
+	fscanf(fp, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
 	       &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
-	fclose(fp);
-	long long idle = a[3] + a[4], total = 0;
-	for (int i = 0; i < 10; i++) total += a[i];
-	total -= pretotal, idle -= previdle;
+	fclose(fp), idle = a[3] + a[4] - idle, total = sumOfArray(a, 10) - total;
 	return (total - idle) * 100.0 / total;
 }
+
+float cpu_usage_loop(FILE *fp, ull *a, ull *prev_idle, ull *prev_total,
+                     int interval) {
+	fflush(stdout), fclose(fp), sleep(interval), fp = fopen("/proc/stat", "r");
+	fscanf(fp, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
+	       &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
+	ull idle = a[3] + a[4], total = 0;
+	for (int i = 0; i < 10; i++) total += a[i];
+	ull totald = total - *prev_total, idled = idle - *prev_idle;
+	*prev_idle = idle, *prev_total = total;
+	return 100.0 * (totald - idled) / totald;
+};
 
 float cpu_temp(char *zone) {
 	char path[100];
@@ -52,15 +69,34 @@ float cpu_temp(char *zone) {
 }
 
 int main(int argc, char *argv[]) {
+	int interval = 1;
 	char *stat = "/proc/stat", *cpuinfo = "/proc/cpuinfo",
 		 *current_profile = "powerprofilesctl get ";
 	if (argc == 1) {
-		printf("<span>%s</span> %.2fGHz %.2f%%\n",
+		printf("<span>%s</span> %.2fGHz %.3g%%\n",
 		       power_profile(current_profile), cpu_clock(cpuinfo),
-		       cpu_usage(stat));
+		       cpu_usage(stat, interval));
 		return 0;
 	} else {
-		int argpos = 0, colored = 0;
+		if (strcmp(argv[1], "-l") == 0) {
+			printf("<span>%s</span> %.2fGHz\n", power_profile(current_profile),
+			       cpu_clock(cpuinfo));
+			interval = 10;
+			ull a[10];
+			FILE *fp = fopen(stat, "r");
+			fscanf(fp, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+			       &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7],
+			       &a[8], &a[9]);
+			ull idle = a[3] + a[4], total = 0;
+			for (int i = 0; i < 10; i++) total += a[i];
+			while (1) {
+				printf("<span>%s</span> %.2fGHz %.2f%%\n",
+				       power_profile(current_profile), cpu_clock(cpuinfo),
+				       cpu_usage_loop(fp, a, &idle, &total, interval));
+			};
+		};
+		int argpos = 0;
+		char colored = 0;
 		for (int i = 1; i < argc; i++)
 			if (strcmp(argv[i], "-C") == 0) {
 				colored = 1;
@@ -69,13 +105,13 @@ int main(int argc, char *argv[]) {
 		while (++argpos < argc) {
 			if (strcmp(argv[argpos], "-C") == 0) continue;
 			else if (strcmp(argv[argpos], "-t") == 0)
-				printf("%.0f°C", cpu_temp(argv[++argpos]));
+				printf("%.3g°C", cpu_temp(argv[++argpos]));
 			else if (strcmp(argv[argpos], "-p") == 0)
 				printf("<span>%s</span>", power_profile(current_profile));
 			else if (strcmp(argv[argpos], "-c") == 0)
 				printf("%.2fGHz", cpu_clock(cpuinfo));
 			else if (strcmp(argv[argpos], "-u") == 0) {
-				float usage = cpu_usage(stat);
+				float usage = cpu_usage(stat, interval);
 				if (colored) {
 					char *color[6] = {
 						"#00ffae", "#04ff00", "#eaff00",
@@ -84,7 +120,7 @@ int main(int argc, char *argv[]) {
 					char index = usage / 100 * 6;
 					printf("<span color='%s'>%.2f%%</span>", color[index],
 					       usage);
-				} else printf("%.2f%%", usage);
+				} else printf("%.3g%%", usage);
 			};
 			if (argpos + 1 < argc) printf(" ");
 		};
