e2df72a26e379cf4cf4f608329577189ec062b85
minhnbnt <minhnbnt@gmail.com>
2023-04-09 13:55:25 +0700

    better perfomance for cpu3

--- .config/waybar/cpu3.c
+++ .config/waybar/cpu3.c
@@ -4,10 +4,9 @@
 
 #define ull unsigned long long
 
-const char *power_profile(const char *path) {
-	char buf[12];
-	FILE *fp = popen(path, "r");
-	fgets(buf, 12, fp), pclose(fp);
+char *power_profile(const char *command) {
+	char *buf;
+	fgets(buf, 12, popen(command, "r"));
 	if (strncmp(buf, "balanced", 8) == 0) {
 		return "&#xf24e;"; // 
 	} else if (strncmp(buf, "performance", 11) == 0) {
@@ -18,11 +17,10 @@ const char *power_profile(const char *path) {
 }
 
 float cpu_temp(const char *zone) {
-	char path[100];
-	sprintf(path, "/sys/class/thermal/%s/temp", zone);
-	FILE *fp = fopen(path, "r");
 	float temp;
-	fscanf(fp, "%f", &temp), fclose(fp);
+	char path[50];
+	sprintf(path, "/sys/class/thermal/%s/temp", zone);
+	fscanf(fopen(path, "r"), "%f", &temp);
 	return temp / 1000;
 }
 
@@ -38,34 +36,28 @@ float cpu_clock(const char *path) {
 	fclose(fp);
 	return sum / core / 1000; // average clock
 }
-float cpu_usage_loop(FILE *fp, ull *a, ull *prev_idle, ull *prev_total,
-                     unsigned delay) {
-	fflush(stdout), fclose(fp), sleep(delay), fp = fopen("/proc/stat", "r");
-	fscanf(fp, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
-	       &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
-	ull idle = a[3] + a[4], total = 0;
-	for (char i = 0; i < 10; i++) total += a[i];
-	ull totald = total - *prev_total, idled = idle - *prev_idle;
-	*prev_idle = idle, *prev_total = total; // for next loop
-	return 100.0 * (totald - idled) / totald;
-};
 
-int main() {
-	char *stat = "/proc/stat", *cpuinfo = "/proc/cpuinfo",
-		 *current_profile = "powerprofilesctl get ", *zone = "thermal_zone1";
-	printf("{\"text\": \"nan\", \"alt\": \"<span>%s</span> %.2fGHz %.3g°C\"}\n",
-	       power_profile(current_profile), cpu_clock(cpuinfo), cpu_temp(zone));
-	ull a[10];
-	FILE *fp = fopen(stat, "r");
-	fscanf(fp, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
+float cpu_usage_loop(ull *a, ull *prev_idle, ull *prev_total) {
+	fscanf(fopen("/proc/stat", "r"),
+	       "cpu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
 	       &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
 	ull idle = a[3] + a[4], total = 0;
 	for (int i = 0; i < 10; i++) total += a[i];
-	while (1)
-		printf("{\"text\": \"%.2f%%\", \"alt\": \"<span>%s</span> %.2fGHz "
+	ull totald = total - *prev_total, idled = idle - *prev_idle;
+	*prev_idle = idle, *prev_total = total; // for next loop
+	return 100.0 * (totald - idled) / totald;
+}
+
+int main() {
+	char *cpuinfo = "/proc/cpuinfo", *current_profile = "powerprofilesctl get ",
+		 *zone = "thermal_zone1";
+	ull a[10], idle = 0, total = 0;
+	while (1) {
+		printf("{\"text\": \"%.0f%%\", \"alt\": \"<span>%s</span> %.2fGHz "
 		       "%.3g°C\"}\n",
-		       cpu_usage_loop(fp, a, &idle, &total, 3),
-		       power_profile(current_profile), cpu_clock(cpuinfo),
-		       cpu_temp(zone));
+		       cpu_usage_loop(a, &idle, &total), power_profile(current_profile),
+		       cpu_clock(cpuinfo), cpu_temp(zone));
+		fflush(stdout), sleep(10); // clear cache and sleep 10s
+	}
 	return 0;
 }
