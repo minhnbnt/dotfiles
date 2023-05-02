bc3fd6ca213d575211ffd387245521b901338944
minhnbnt <minhnbnt@gmail.com>
2023-05-02 20:51:49 +0700

    just commit

--- .config/nvim/lua/lsp/luasnip.lua
+++ .config/nvim/lua/lsp/luasnip.lua
@@ -1,13 +1,12 @@
 require("luasnip").add_snippets(nil, {
-	html = require("lsp.snippet.html"),
+	--html = require("lsp.snippet.html"),
 	c = require("lsp.snippet.c"),
 	cpp = require("lsp.snippet.cpp"),
 })
 
+require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({
-	--path = "~/.config/nvim/lua/lsp/snippet/emmet.json",
-})
 
 require("luasnip").filetype_extend("all", { "_" })
 require("luasnip").filetype_extend("cpp", { "c" })
+require("luasnip").filetype_extend("typescript", { "javascript" })
