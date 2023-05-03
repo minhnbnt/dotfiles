087ea72ebcbe77983d92154c38b1df4d43d59840
minhnbnt <minhnbnt@gmail.com>
2023-05-03 19:16:33 +0700

    delete old comment

--- .config/nvim/lua/lsp/cmp.lua
+++ .config/nvim/lua/lsp/cmp.lua
@@ -1,32 +1,6 @@
 -- Set up nvim-cmp.
 local cmp = require("cmp")
 
-local type_comparator = function(conf)
-	local lsp_types = require("cmp.types").lsp
-	return function(entry1, entry2)
-		if entry1.source.name ~= "nvim_lsp" then
-			if entry2.source.name == "nvim_lsp" then
-				return false
-			else
-				return nil
-			end
-		end
-		local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
-		local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
-
-		local priority1 = conf.kind_priority[kind1] or 0
-		local priority2 = conf.kind_priority[kind2] or 0
-		if priority1 == priority2 then
-			return nil
-		end
-		return priority2 < priority1
-	end
-end
-
-local label_comparator = function(entry1, entry2)
-	return entry1.completion_item.label < entry2.completion_item.label
-end
-
 local kind_icons = {
 	Copilot = "",
 	Text = "",
@@ -75,38 +49,6 @@ local source_icons = {
 
 cmp.setup({
 	preselect = cmp.PreselectMode.None,
-	--comparators = {
-	--	type_comparator({
-	--		kind_priority = {
-	--			Field = 11,
-	--			Property = 11,
-	--			Constant = 10,
-	--			Enum = 10,
-	--			EnumMember = 10,
-	--			Event = 10,
-	--			Function = 10,
-	--			Method = 10,
-	--			Operator = 10,
-	--			Reference = 10,
-	--			Struct = 10,
-	--			Variable = 9,
-	--			File = 8,
-	--			Folder = 8,
-	--			Class = 5,
-	--			Color = 5,
-	--			Module = 5,
-	--			Keyword = 0,
-	--			Constructor = 1,
-	--			Interface = 1,
-	--			Snippet = 0,
-	--			Text = 1,
-	--			TypeParameter = 1,
-	--			Unit = 1,
-	--			Value = 1,
-	--		},
-	--	}),
-	--	label_comparator,
-	--},
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
@@ -123,6 +65,7 @@ cmp.setup({
 	},
 	window = {
 		--completion = cmp.config.window.bordered(),
+		--documentation = cmp.config.window.bordered(),
 		documentation = {
 			max_height = 30,
 		},
@@ -139,7 +82,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<ESC>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Set `select` to `false` to only confirm explicitly selected items.
 		["<Tab>"] = cmp.mapping.confirm({ select = true }),
 		["<Down>"] = cmp.mapping.select_next_item(),
 		["<Up>"] = cmp.mapping.select_prev_item(),
@@ -188,7 +131,7 @@ cmp.setup({
 			end
 			-- Kind icons
 			-- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-			vim_item.dup = { nvim_lsp = 0 }
+			--vim_item.dup = { nvim_lsp = 0 }
 			local icon = kind_icons[vim_item.kind] or " "
 			vim_item.kind = string.format("%s %s", icon, vim_item.kind)
 			-- source icons
@@ -236,5 +179,4 @@ cmp.setup.cmdline(":", {
 })
 
 vim.cmd("hi CmpItemMenu cterm=bold gui=bold")
-
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
