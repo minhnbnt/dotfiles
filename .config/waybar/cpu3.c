31a99a7e610b1a5836019f90671ae4f94111a268
minhnbnt <minhnbnt@gmail.com>
2023-05-03 19:21:07 +0700

    disable power profile function

--- .config/waybar/cpu3.c
+++ .config/waybar/cpu3.c
@@ -4,9 +4,9 @@
 
 #define ull unsigned long long
 
-char *power_profile(const char *command) {
-	char *buf;
-	fgets(buf, 12, popen(command, "r"));
+char *power_profile(void) {
+	char buf[12]; // store power profile
+	fgets(buf, 12, popen("powerprofilesctl get", "r"));
 	if (strncmp(buf, "balanced", 8) == 0) {
 		return "&#xf24e;"; // 
 	} else if (strncmp(buf, "performance", 11) == 0) {
@@ -18,18 +18,18 @@ char *power_profile(const char *command) {
 
 float cpu_temp(const char *zone) {
 	float temp;
-	char path[50];
+	char path[50]; // path to thermal_zone
 	sprintf(path, "/sys/class/thermal/%s/temp", zone);
 	fscanf(fopen(path, "r"), "%f", &temp);
 	return temp / 1000;
 }
 
-float cpu_clock(const char *path) {
-	FILE *fp = fopen(path, "r");
+float cpu_clock(void) {
+	FILE *fp = fopen("/proc/cpuinfo", "r");
 	char buf[100], core = 0;
 	float cpu_clock, sum = 0;
 	while (fgets(buf, 100, fp) != NULL)
-		if (strncmp(buf, "cpu MHz", 7) == 0) {
+		if (strncmp(buf, "cpu MHz", 7) == 0) { // limit length to 7
 			sscanf(buf, "cpu MHz\t: %f", &cpu_clock);
 			core++, sum += cpu_clock;
 		};
@@ -39,8 +39,8 @@ float cpu_clock(const char *path) {
 
 float cpu_usage_loop(ull *a, ull *prev_idle, ull *prev_total) {
 	fscanf(fopen("/proc/stat", "r"),
-	       "cpu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &a[0],
-	       &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
+	       "cpu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", //
+	       &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
 	ull idle = a[3] + a[4], total = 0;
 	for (int i = 0; i < 10; i++) total += a[i];
 	ull totald = total - *prev_total, idled = idle - *prev_idle;
@@ -48,15 +48,13 @@ float cpu_usage_loop(ull *a, ull *prev_idle, ull *prev_total) {
 	return 100.0 * (totald - idled) / totald;
 }
 
-int main() {
-	char *cpuinfo = "/proc/cpuinfo", *current_profile = "powerprofilesctl get ",
-		 *zone = "thermal_zone1";
-	ull a[10], idle = 0, total = 0;
+int main(void) {
+	char *zone = "thermal_zone1";   // default zone
+	ull a[10], idle = 0, total = 0; // for cpu usage
 	while (1) {
-		printf("{\"text\": \"%.0f%%\", \"alt\": \"<span>%s</span> %.2fGHz "
-		       "%.3g°C\"}\n",
-		       cpu_usage_loop(a, &idle, &total), power_profile(current_profile),
-		       cpu_clock(cpuinfo), cpu_temp(zone));
+		printf("{\"text\": \"%.0f%%\", \"alt\": \"" /*"<span>%s</span> "*/
+		       "%.2fGHz %.3g°C\"}\n",
+		       cpu_usage_loop(a, &idle, &total), /*power_profile(),*/ cpu_clock(), cpu_temp(zone));
 		fflush(stdout), sleep(10); // clear cache and sleep 10s
 	}
 	return 0;
