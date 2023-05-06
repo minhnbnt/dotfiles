5caa3014677db778f1e70f72f41d0532b4d31eb5
minhnbnt <minhnbnt@gmail.com>
2023-05-06 11:08:58 +0700

    update lualine

--- .config/nvim/lua/ui/lualine.lua
+++ .config/nvim/lua/ui/lualine.lua
@@ -34,11 +34,92 @@ local server_name = function()
 	end
 	if #attached > 3 then -- show 2 servers + "more"
 		return table.concat(display, ", ") .. " + " .. #attached - 2 .. " more"
-	end
-	if attached[3] ~= nil then
+	elseif attached[3] ~= nil then
 		table.insert(display, attached[3])
 	end -- show all servers
 	return table.concat(display, ", ")
+end
+
+local filetype = function() -- just copy from lualine
+	local lualine_require = require("lualine_require")
+	local modules = lualine_require.lazy_require({
+		highlight = "lualine.highlight",
+		utils = "lualine.utils.utils",
+	})
+	local M = lualine_require.require("lualine.component"):extend()
+
+	local default_options = {
+		colored = true,
+		icon_only = false,
+	}
+
+	function M:init(options)
+		M.super.init(self, options)
+		self.options = vim.tbl_deep_extend("keep", self.options or {}, default_options)
+		self.icon_hl_cache = {}
+	end
+
+	function M.update_status()
+		local ft = vim.fn.expand("%:e") -- my edit
+		if ft == "" then
+			ft = vim.bo.filetype or ""
+		end
+		return modules.utils.stl_escape(ft)
+	end
+
+	function M:apply_icon()
+		if not self.options.icons_enabled then
+			return
+		end
+
+		local icon, icon_highlight_group
+		local ok, devicons = pcall(require, "nvim-web-devicons")
+		if ok then
+			icon, icon_highlight_group = devicons.get_icon(vim.fn.expand("%:t"))
+			if icon == nil then
+				icon, icon_highlight_group = devicons.get_icon_by_filetype(vim.bo.filetype)
+			end
+
+			if icon == nil and icon_highlight_group == nil then
+				icon = ""
+				icon_highlight_group = "DevIconDefault"
+			end
+			if self.options.colored then
+				local highlight_color = modules.utils.extract_highlight_colors(icon_highlight_group, "fg")
+				if highlight_color then
+					local default_highlight = self:get_default_hl()
+					local icon_highlight = self.icon_hl_cache[highlight_color]
+					if
+						not icon_highlight or not modules.highlight.highlight_exists(icon_highlight.name .. "_normal")
+					then
+						icon_highlight = self:create_hl({ fg = highlight_color }, icon_highlight_group)
+						self.icon_hl_cache[highlight_color] = icon_highlight
+					end
+
+					icon = self:format_hl(icon_highlight) .. icon .. default_highlight
+				end
+			end
+		else
+			ok = vim.fn.exists("*WebDevIconsGetFileTypeSymbol")
+			if ok ~= 0 then
+				icon = vim.fn.WebDevIconsGetFileTypeSymbol()
+			end
+		end
+
+		if not icon then
+			return
+		end
+
+		if self.options.icon_only then
+			self.status = icon
+		elseif type(self.options.icon) == "table" and self.options.icon.align == "right" then
+			self.status = self.status .. " " .. icon
+		else
+			self.status = icon .. " " .. self.status
+		end
+	end
+
+	return M
 end
 
 local config = {
@@ -166,11 +247,9 @@ local conditions = {
 	end,
 	hide_in_width = function(index)
 		local widths = { 115, 100, 95, 88, 78, 75, 70, 67, 55, 40 }
-		if index > #widths then
-			return -- index out of bounds
-		end
+		local width = widths[index] or 0 -- fallback to 0
 		return function()
-			return vim.fn.winwidth(0) > widths[index]
+			return vim.fn.winwidth(0) > width
 		end
 	end,
 }
@@ -231,7 +310,7 @@ ins.left({
 	}
 end)]]
 ins.left({
-	require("ui.test"),
+	filetype(),
 	icon_only = true,
 })
 
@@ -395,7 +474,11 @@ ins.right({
 
 ins.right({
 	require("noice").api.status.command.get,
-	cond = require("noice").api.status.command.has,
+	cond = function() -- combine 2 conditions
+		local has_command = require("noice").api.status.command.has
+		local hide_in_width = conditions.hide_in_width(10)
+		return hide_in_width() and has_command()
+	end,
 })
 
 ins.right({
