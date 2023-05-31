f9d138d83016bd4b2c3016643fcb6f1bc9e06f77
minhnbnt <minhnbnt@gmail.com>
2023-05-31 21:07:07 +0700

    add typescript, ulimit for coderunner

--- .config/nvim/lua/coding/coderunner.lua
+++ .config/nvim/lua/coding/coderunner.lua
@@ -5,6 +5,7 @@ local function get_command(compile, run)
 		compile = compile .. " && "
 	end
 	return {
+		"ulimit -d 524288;", -- limit memory to 512MB
 		'cd "$dir" && ' .. compile .. "if [[ -f input.txt ]];", -- check if input.txt exists
 		'then cat input.txt && read -p "Run with input.txt? [Y/n]: " answer;', -- ask if user wants to run with input.txt
 		'while [[ $answer != [yYnN] ]]; do read -p "Invalid option: " answer; done;', -- check if answer is valid
@@ -28,13 +29,14 @@ require("code_runner").setup({
 		),
 		c = get_command("clang $fileName -lm -o $fileNameWithoutExt", "./$fileNameWithoutExt"),
 		cpp = get_command("clang++ $fileName -o $fileNameWithoutExt", "./$fileNameWithoutExt"),
-		go = get_command(nil, "go run $fileName"),
+		go = get_command("go build $fileName", "./$fileNameWithoutExt"),
 		html = 'cd "$dir" && live-server --open=$fileName',
-		java = get_command("javac $fileName", "java $fileNameWithoutExt"),
+		java = get_command(nil, "java -cp . $fileName"),
 		javascript = get_command(nil, "node $fileName"),
 		lua = get_command(nil, "lua $fileName"),
 		python = get_command(nil, "python $fileName"),
 		rust = get_command("rustc $fileName", "./$fileNameWithoutExt"),
 		sh = get_command(nil, "bash $fileName"),
+		typescript = get_command("tsc $fileName", "node $fileNameWithoutExt.js"),
 	},
 })
