f6732de339b0e730ab7ae92448823f6f1280863e
minhnbnt <minhnbnt@gmail.com>
2023-07-01 20:44:58 +0700

    better code for lualine

--- .config/nvim/lua/ui/lualine.lua
+++ .config/nvim/lua/ui/lualine.lua
@@ -1,5 +1,3 @@
-local copilot_active = false
-
 local server_name = function()
 	local attached = {} -- list of attached servers
 	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
@@ -21,169 +19,53 @@ local server_name = function()
 				table.insert(attached, client.name)
 			end
 		elseif client.name == "copilot" then
-			copilot_active = true -- for later
+			vim.b.copilot_active = true -- for later
 		end
 	end
-	local display = {}
-	for i = 1, 2 do -- insert first 2 servers
-		if attached[i] ~= nil then
-			table.insert(display, attached[i])
-		else -- no more servers
-			break
-		end
+	local num_attached = #attached
+	if num_attached > 3 then
+		local str = table.concat(attached, ", ", 1, 2)
+		return str .. " + " .. num_attached - 2 .. " more"
 	end
-	if #attached > 3 then -- show 2 servers + "more"
-		return table.concat(display, ", ") .. " + " .. #attached - 2 .. " more"
-	elseif attached[3] ~= nil then
-		table.insert(display, attached[3])
-	end -- show all servers
-	return table.concat(display, ", ")
+	return table.concat(attached, ", ")
 end
 
-local filetype = function() -- just copy from lualine
-	local lualine_require = require("lualine_require")
-	local modules = lualine_require.lazy_require({
+local filetype = require("lualine.components.filetype")
+
+function filetype.update_status()
+	local modules = require("lualine_require").lazy_require({
 		highlight = "lualine.highlight",
 		utils = "lualine.utils.utils",
 	})
-	local M = lualine_require.require("lualine.component"):extend()
 
-	local default_options = {
-		colored = true,
-		icon_only = false,
-	}
-
-	function M:init(options)
-		M.super.init(self, options)
-		self.options = vim.tbl_deep_extend("keep", self.options or {}, default_options)
-		self.icon_hl_cache = {}
+	local ft = vim.fn.expand("%:e")
+	if ft == "" then
+		ft = vim.bo.filetype or ""
 	end
-
-	function M.update_status()
-		local ft = vim.fn.expand("%:e") -- my edit
-		if ft == "" then
-			ft = vim.bo.filetype or ""
-		end
-		return modules.utils.stl_escape(ft)
-	end
-
-	function M:apply_icon()
-		if not self.options.icons_enabled then
-			return
-		end
-
-		local icon, icon_highlight_group
-		local ok, devicons = pcall(require, "nvim-web-devicons")
-		if ok then
-			icon, icon_highlight_group = devicons.get_icon(vim.fn.expand("%:t"))
-			if icon == nil then
-				icon, icon_highlight_group = devicons.get_icon_by_filetype(vim.bo.filetype)
-			end
-
-			if icon == nil and icon_highlight_group == nil then
-				icon = ""
-				icon_highlight_group = "DevIconDefault"
-			end
-			if self.options.colored then
-				local highlight_color = modules.utils.extract_highlight_colors(icon_highlight_group, "fg")
-				if highlight_color then
-					local default_highlight = self:get_default_hl()
-					local icon_highlight = self.icon_hl_cache[highlight_color]
-					if
-						not icon_highlight or not modules.highlight.highlight_exists(icon_highlight.name .. "_normal")
-					then
-						icon_highlight = self:create_hl({ fg = highlight_color }, icon_highlight_group)
-						self.icon_hl_cache[highlight_color] = icon_highlight
-					end
-
-					icon = self:format_hl(icon_highlight) .. icon .. default_highlight
-				end
-			end
-		else
-			ok = vim.fn.exists("*WebDevIconsGetFileTypeSymbol")
-			if ok ~= 0 then
-				icon = vim.fn.WebDevIconsGetFileTypeSymbol()
-			end
-		end
-
-		if not icon then
-			return
-		end
-
-		if self.options.icon_only then
-			self.status = icon
-		elseif type(self.options.icon) == "table" and self.options.icon.align == "right" then
-			self.status = self.status .. " " .. icon
-		else
-			self.status = icon .. " " .. self.status
-		end
-	end
-
-	return M
+	return modules.utils.stl_escape(ft)
 end
 
 local config = {
-	options = {
-		-- disable sections and component separators
-		component_separators = "",
-		section_separators = "",
-		theme = {
-			normal = {
-				a = { fg = "#303446", bg = "#1E66F5" },
-				b = { fg = "#1E66F5", bg = "#414559" },
-				c = { fg = "#E6E9EF", bg = "#181825" },
-			},
-			insert = {
-				a = { fg = "#303446", bg = "#40A02B" },
-				b = { fg = "#40A02B", bg = "#414559" },
-				c = { fg = "#E6E9EF", bg = "#181825" },
-			},
-			visual = {
-				a = { fg = "#303446", bg = "#DF8E1D" },
-				b = { fg = "#DF8E1D", bg = "#414559" },
-				c = { fg = "#E6E9EF", bg = "#181825" },
-			},
-			command = {
-				a = { fg = "#303446", bg = "#EA76CB" },
-				b = { fg = "#EA76CB", bg = "#414559" },
-				c = { fg = "#E6E9EF", bg = "#181825" },
-			},
-			replace = {
-				a = { fg = "#303446", bg = "#E64553" },
-				b = { fg = "#E64553", bg = "#414559" },
-				c = { fg = "#E6E9EF", bg = "#181825" },
-			},
-			terminal = {
-				a = { fg = "#303446", bg = "#8839EF" },
-				b = { fg = "#8839EF", bg = "#414559" },
-				c = { fg = "#E6E9EF", bg = "#181825" },
-			},
-		},
-		padding = 1, -- padding between components
-	},
 	sections = {
 		lualine_a = {},
+		lualine_y = {},
 		lualine_b = {
 			{
 				function() -- show identation level
 					local chars = { "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█" }
 					local tab_width = vim.api.nvim_buf_get_option(0, "tabstop")
-					if tab_width > 8 then
-						return "█"
-					end
-					return chars[tab_width]
+					return chars[tab_width] or "█"
 				end,
 				color = { bg = "#363A4F" },
 				padding = 0,
 			},
 		},
-		lualine_y = {},
 		lualine_z = {
 			{
 				function() -- show current position in file
 					local current_line = vim.fn.line(".")
 					local total_lines = vim.fn.line("$")
-					--local chars = { " ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
+					-- local chars = { " ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
 					local chars = { "█", "▇", "▆", "▅", "▄", "▃", "▂", "▁", " " }
 					local line_ratio = current_line / total_lines
 					local index = math.ceil(line_ratio * #chars)
@@ -198,8 +80,6 @@ local config = {
 		lualine_x = {},
 	},
 	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
 		lualine_c = {
 			{
 				"filename",
@@ -222,8 +102,6 @@ local config = {
 				padding = { left = 1, right = 2 },
 			},
 		},
-		lualine_y = {},
-		lualine_z = {},
 	},
 }
 
@@ -253,6 +131,7 @@ local conditions = {
 		end
 	end,
 }
+
 ins.left({
 	function() --[[
 		local ft = { "html", "xhtml", "xml", "typescriptreact", "javascriptreact" }
@@ -260,7 +139,7 @@ ins.left({
 			vim.cmd("se tabstop=2 shiftwidth=2 softtabstop=2")
 		else
 			vim.cmd("se tabstop=4 shiftwidth=4 softtabstop=4")
-		end]]
+		end ]]
 		return " "
 	end,
 	padding = 0,
@@ -273,7 +152,7 @@ ins.left({
 
 ins.left({
 	function()
-		if copilot_active then
+		if vim.b.copilot_active then
 			return ""
 		end
 		return ""
@@ -296,21 +175,8 @@ ins.left({
 	padding = { left = 1, right = 0 },
 })
 
---[[ins.left(function()
-	local name, ext = vim.fn.expand("%:t"), vim.fn.expand("%:e")
-	local icon, hl = require("nvim-web-devicons").get_icon(name, ext, { default = false })
-	return {
-		function()
-			if icon then
-				return icon
-			end
-			return ""
-		end,
-		color = { fg = hl },
-	}
-end)]]
 ins.left({
-	filetype(),
+	filetype,
 	icon_only = true,
 })
 
@@ -336,16 +202,6 @@ ins.left({
 	cond = conditions.hide_in_width(7),
 })
 
---[[ins.left({
-	function()
-		if vim.fn.winwidth(0) > 115 or vim.api.nvim_get_mode().mode == "n" or vim.o.showmode then
-			return ""
-		end
-		return vim.api.nvim_get_mode().mode
-	end,
-	color = { gui = "bold" },
-	padding = { left = 1, right = 0 },
-})]]
 ins.left({
 	function()
 		if vim.bo.modified then
@@ -373,54 +229,22 @@ ins.left({
 })
 
 ins.left({
-	function()
-        -- stylua: ignore
-        local Mode = {
-            ['n']     = '',
-            ['niI']   = '',
-            ['niR']   = '',
-            ['niV']   = '',
-            ['nt']    = '',
-            ['ntT']   = '',
-            ['no']    = '-- OPERATOR PENDING --',
-            ['nov']   = '-- OPERATOR PENDING --',
-            ['noV']   = '-- OPERATOR PENDING --',
-            ['no\22'] = '-- OPERATOR PENDING --',
-            ['v']     = '-- VISUAL --',
-            ['vs']    = '-- VISUAL --',
-            ['V']     = '-- VISUAL LINE --',
-            ['Vs']    = '-- VISUAL LINE --',
-            ['\22']   = '-- VISUAL BLOCK --',
-            ['\22s']  = '-- VISUAL BLOCK --',
-            ['s']     = '-- SELECT --',
-            ['S']     = '-- SELECT LINE --',
-            ['\19']   = '-- SELECT BLOCK --',
-            ['i']     = '-- INSERT --',
-            ['ic']    = '-- INSERT --',
-            ['ix']    = '-- INSERT --',
-            ['R']     = '-- REPLACE --',
-            ['Rc']    = '-- REPLACE --',
-            ['Rx']    = '-- REPLACE --',
-            ['Rv']    = '-- VISUAL REPLACE --',
-            ['Rvc']   = '-- VISUAL REPLACE --',
-            ['Rvx']   = '-- VISUAL REPLACE --',
-            ['c']     = '-- COMMAND --',
-            ['cv']    = '-- EX --',
-            ['ce']    = '-- EX --',
-            ['r']     = '-- REPLACE --',
-            ['rm']    = '-- MORE --',
-            ['r?']    = '-- CONFIRM --',
-            ['!']     = '-- SHELL --',
-            ['t']     = '-- TERMINAL --',
-        }
-		if vim.o.showmode then
+	"mode",
+	fmt = function(str)
+		-- stylua: ignore
+		local map = {
+			["O-PENDING"] = "OPERATOR PENDING",
+			["V-BLOCK"]   = "VISUAL BLOCK",
+			["V-LINE"]    = "VISUAL LINE",
+			["V-REPLACE"] = "VISUAL REPLACE",
+			["S-BLOCK"]   = "SELECT BLOCK",
+			["S-LINE"]    = "SELECT LINE",
+		}
+		local mode = map[str] or str
+		if mode == "NORMAL" then
 			return ""
 		end
-		local mode_code = vim.api.nvim_get_mode().mode
-		if Mode[mode_code] == nil then
-			return mode_code
-		end
-		return Mode[mode_code]
+		return "-- " .. mode .. " --"
 	end,
 	color = { gui = "bold" },
 	cond = conditions.hide_in_width(1),
@@ -467,14 +291,18 @@ ins.right({
 	cond = conditions.hide_in_width(10),
 })
 
-ins.right({
-	require("noice").api.status.command.get,
-	cond = function() -- combine 2 conditions
-		local has_command = require("noice").api.status.command.has
-		local hide_in_width = conditions.hide_in_width(10)
-		return hide_in_width() and has_command()
-	end,
-})
+local has_noice, noice = pcall(require, "noice")
+
+if has_noice then
+	ins.right({
+		noice.api.status.command.get,
+		cond = function() -- combine 2 conditions
+			local has_command = noice.api.status.command.has
+			local hide_in_width = conditions.hide_in_width(10)
+			return hide_in_width() and has_command()
+		end,
+	})
+end
 
 ins.right({
 	function()
@@ -490,5 +318,44 @@ ins.right({
 	end,
 	padding = 0,
 })
+
+config.options = {
+	-- disable sections and component separators
+	component_separators = "",
+	section_separators = "",
+	padding = 1, -- padding between components
+	theme = {
+		normal = {
+			a = { fg = "#303446", bg = "#1E66F5" },
+			b = { fg = "#1E66F5", bg = "#414559" },
+			c = { fg = "#E6E9EF", bg = "#181825" },
+		},
+		insert = {
+			a = { fg = "#303446", bg = "#40A02B" },
+			b = { fg = "#40A02B", bg = "#414559" },
+			c = { fg = "#E6E9EF", bg = "#181825" },
+		},
+		visual = {
+			a = { fg = "#303446", bg = "#DF8E1D" },
+			b = { fg = "#DF8E1D", bg = "#414559" },
+			c = { fg = "#E6E9EF", bg = "#181825" },
+		},
+		command = {
+			a = { fg = "#303446", bg = "#EA76CB" },
+			b = { fg = "#EA76CB", bg = "#414559" },
+			c = { fg = "#E6E9EF", bg = "#181825" },
+		},
+		replace = {
+			a = { fg = "#303446", bg = "#E64553" },
+			b = { fg = "#E64553", bg = "#414559" },
+			c = { fg = "#E6E9EF", bg = "#181825" },
+		},
+		terminal = {
+			a = { fg = "#303446", bg = "#8839EF" },
+			b = { fg = "#8839EF", bg = "#414559" },
+			c = { fg = "#E6E9EF", bg = "#181825" },
+		},
+	},
+}
 
 require("lualine").setup(config)
