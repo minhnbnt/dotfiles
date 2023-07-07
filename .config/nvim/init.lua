b1c3d92a3effd698365f0efac1b3efec7a8f4afd
minhnbnt <minhnbnt@gmail.com>
2023-07-07 20:35:07 +0700

    add minimal config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,15 @@ else
 	require("options")
 	require("plug")
 end
+
+-- used for testing
+--[[
+local function start()
+	return require("packer").startup(function(use)
+		
+		use("wbthomason/packer.nvim")
+	end)
+end
+
+start()
+]]
