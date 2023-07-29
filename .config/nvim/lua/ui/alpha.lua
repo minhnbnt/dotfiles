8099520079eab1cb7d0eee7ad3333845de0a067d
minhnbnt <minhnbnt@gmail.com>
2023-07-29 12:30:43 +0700

    edit icon for neovim

--- .config/nvim/lua/ui/alpha.lua
+++ .config/nvim/lua/ui/alpha.lua
@@ -32,18 +32,18 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("n", "  New file", ":ene <CR>"),
 	dashboard.button("o", "  Open file", ":Telescope file_browser hidden=true grouped=true <CR>"),
-	dashboard.button("f", "  Find file", ":Telescope find_files hidden=true <CR>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files hidden=true <CR>"),
 	dashboard.button("p", "  Find project", ":lua require'telescope'.extensions.projects.projects{} <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button(
 		"c",
 		"  Configuration",
 		":Telescope file_browser hidden=true grouped=true path=~/.config/nvim/ <CR>"
 	),
-	dashboard.button("g", "  Live grep", ":Telescope live_grep <CR>"),
+	dashboard.button("g", "  Live grep", ":Telescope live_grep <CR>"),
 	dashboard.button("t", "  Tutor", ":Tutor <CR>"),
 	dashboard.button("h", "  Help", ":tab h <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa <CR>"),
 }
 
 local function footer()
