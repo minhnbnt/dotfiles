d6aad9244e59f3e7ce68cde2e7df3da094553a22
minhnbnt <minhnbnt@gmail.com>
2023-08-07 07:43:00 +0700

    add wireplumber module

---
+++ .config/eww/bar/src/wireplumber.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ID "@DEFAULT_AUDIO_SINK@"
+
+float get_volume(char *mute_status) {
+
+	float vol;
+	FILE *cmd = popen("wpctl get-volume " ID, "r");
+	fscanf(cmd, "Volume: %f %s", &vol, mute_status);
+
+	return 100 * vol;
+}
+
+const char *get_icon(float vol) {
+	if (vol == 0) return "";
+	else if (vol < 33) return "";
+	else if (vol < 66) return "";
+	else return "";
+}
+
+int main(int argc, char *argv[]) {
+
+	if (argc == 2) {
+
+		float vol = atof(argv[1]) / 100;
+		char *command = calloc(50, sizeof(char));
+
+		sprintf(command, "wpctl set-volume %s %f", ID, vol);
+		system(command);
+
+		return 0;
+	}
+
+	char *mute_status = calloc(10, sizeof(char));
+	float vol = get_volume(mute_status);
+
+	if (strcmp(mute_status, "[MUTED]") == 0) {
+		printf("{ \"icon\": \"\", \"vol\": \"%.0f\", \"muted\": true,"
+		       " \"tooltip\": \"Volume: muted\" }\n",
+		       vol);
+		return 0;
+	}
+
+	printf("{ \"icon\": \"%s\", \"vol\": \"%.0f\", \"muted\": false,"
+	       " \"tooltip\": \"Volume: %.0f%%\"} \n",
+	       get_icon(vol), vol, vol);
+
+	return 0;
+}
