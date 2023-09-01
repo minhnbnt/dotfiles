774c79959aa3bcdc55b9038c69de3be5fdb4fb7a
minhnbnt <minhnbnt@gmail.com>
2023-09-01 08:44:42 +0700

    better msg width for nvim-notify

--- .config/nvim/lua/plugins/ui/notify.lua
+++ .config/nvim/lua/plugins/ui/notify.lua
@@ -3,6 +3,13 @@ require("notify").setup({
 	render = "compact",
 	stages = "slide",
 	top_down = false,
+
+	max_height = function()
+		return math.floor(vim.o.lines * 0.75)
+	end,
+	max_width = function()
+		return math.floor(vim.o.columns * 0.75)
+	end,
 })
 
 vim.notify = function(msg, ...)
