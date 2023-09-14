12f4c5cb84450135d1878c7dc5eb2ea865643418
minhnbnt <minhnbnt@gmail.com>
2023-09-14 08:36:45 +0700

    update clock tooltip

--- .config/eww/bar/src/calendar.c
+++ .config/eww/bar/src/calendar.c
@@ -3,14 +3,22 @@
 
 int main(void) {
 
+	const char *day_of_week[] = { "Sun", "Mon", "Tue", "Wed",
+		                          "Thu", "Fri", "Sat" };
+
+	const char *month[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+		                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
 	time_t raw_time = time(NULL);
 	struct tm *time_info = localtime(&raw_time);
 
-	printf("{ \"hour\": \"%02d\", \"minute\": \"%02d\", \"second\": \"%02d\", "
-	       "\"day\": \"%d\", \"month\": \"%d\", \"year\": \"%d\" }\n",
-	       time_info->tm_hour, time_info->tm_min, time_info->tm_sec,
-	       time_info->tm_mday, time_info->tm_mon + 1,
-	       time_info->tm_year + 1900);
+	printf("{ \"hour\": \"%02d\", \"minute\": \"%02d\", \"second\": \"%02d\", ",
+	       time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
+
+	printf(" \"tooltip\": \"%s, %d %s %d %02d:%02d:%02d\" }\n",
+	       day_of_week[time_info->tm_wday], time_info->tm_mday,
+	       month[time_info->tm_mon], time_info->tm_year + 1900,
+	       time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
 
 	return 0;
 }
