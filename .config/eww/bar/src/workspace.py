fd4f2ed07b44fd89d96dc49ac4099ac48d382593
minhnbnt <minhnbnt@gmail.com>
2023-10-02 19:50:52 +0700

    use format string

--- .config/eww/bar/src/workspace.py
+++ .config/eww/bar/src/workspace.py
@@ -11,7 +11,7 @@ if argv_len > 2:
     print("Too many arguments")
     sys.exit(1)
 
-elif argv_len == 2:
+if argv_len == 2:
     if sys.argv[1] == "up":
         os.system("hyprctl dispatch workspace e-1")
     elif sys.argv[1] == "down":
@@ -46,7 +46,7 @@ def update_workspace(buf):
 
     changed = False
 
-    def get_number(line):
+    def get_number(line: str):
         return int(line.split(">>")[1])
 
     for line in buf.splitlines():
@@ -54,11 +54,11 @@ def update_workspace(buf):
             active_workspace = get_number(line)
             changed = True
 
-        if line.startswith("createworkspace>>"):
+        elif line.startswith("createworkspace>>"):
             visible_workspaces.add(get_number(line))
             changed = True
 
-        if line.startswith("destroyworkspace>>"):
+        elif line.startswith("destroyworkspace>>"):
             workspace_id = get_number(line)
 
             if workspace_id in visible_workspaces:
@@ -71,14 +71,16 @@ def update_workspace(buf):
 def print_widget():
     visible_set = set(sorted(visible_workspaces))
 
-    print('(eventbox :onscroll "python3', "'" + __file__ + "'", '{}"', end=" ")
+    print("; This is start")
+
+    print('(eventbox :onscroll "python3', f"'{__file__}'", '{}"', end=" ")
     print('(box :class "works" :orientation "v" :space-evenly false', end="")
 
     # print all workspaces in icons list
     for id, icon in enumerate(icons):
         id = id + 1
 
-        print(' (button :tooltip "Workspace {}"'.format(id), end=" ")
+        print(f' (button :tooltip "Workspace {id}"', end=" ")
 
         print(':onclick "hyprctl dispatch workspace', id, end='" ')
         print(':onrightclick "hyprctl dispatch workspace', id, end='" ')
@@ -92,14 +94,14 @@ def print_widget():
         if id in visible_set:
             visible_set.remove(id)
 
-        print(':class "{}" "{}"'.format(button_class, icon), end=")")
+        print(f':class "{button_class}" "{icon}"', end=")")
 
     # if there are any visible workspaces that are not in the icons list
     for id in visible_set:
-        print(' (button :tooltip "Workspace {}"'.format(id), end=" ")
+        print(f' (button :tooltip "Workspace {id}"', end=" ")
 
         if id == active_workspace:
-            print(':class "active" "' + other_icon, end='")')
+            print(f':class "active" "{other_icon}', end='")')
             continue
 
         # visible workspace not in icons list
@@ -114,7 +116,7 @@ def print_widget():
 
 
 signature = os.environ["HYPRLAND_INSTANCE_SIGNATURE"]
-sever_address = "/tmp/hypr/" + signature + "/.socket2.sock"
+sever_address = f"/tmp/hypr/{signature}/.socket2.sock"
 
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 sock.connect(sever_address)
@@ -124,8 +126,5 @@ print_widget()
 while True:
     buf = sock.recv(1024).decode()
 
-    if not buf:
-        continue
-
-    if update_workspace(buf):
+    if buf and update_workspace(buf):
         print_widget()
