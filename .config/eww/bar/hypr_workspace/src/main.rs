7f7db5d5039365aea4a46304f63c70c8a698d226
minhnbnt <minhnbnt@gmail.com>
2023-10-10 08:34:35 +0700

    fix: use Option and some fixes

--- .config/eww/bar/hypr_workspace/src/main.rs
+++ .config/eww/bar/hypr_workspace/src/main.rs
@@ -3,14 +3,10 @@ use std::io::{BufRead, BufReader, Read, Write};
 use std::os::unix::net::UnixStream;
 use std::path::PathBuf;
 
-const OTHER_ICON: &str = "";
-const ICONS: [&str; 7] = ["", "", "", "", "", "", ""];
+pub fn print_workspaces(exec_path: &PathBuf, workspaces: &BTreeSet<u8>, focused: &u8) {
+	const OTHER_ICON: &str = "";
+	const ICONS: [&str; 7] = ["", "", "", "", "", "", ""];
 
-pub fn print_workspaces(
-	exec_path: &PathBuf,
-	workspaces: &BTreeSet<u8>,
-	focused: &u8,
-) {
 	let mut workspaces = workspaces.clone();
 
 	print!("(eventbox :onscroll \"{} {{}}\" ", exec_path.display());
@@ -19,14 +15,14 @@ pub fn print_workspaces(
 	for (index, icon) in ICONS.iter().enumerate() {
 		let index = index as u8 + 1;
 
-		print!(" (button :tooltip 'Workspace {}' ", index);
+		print!(" (button :tooltip 'Workspace {index}' ");
 
-		print!(":onclick 'hyprctl dispatch workspace {}' ", index);
-		print!(":onrightclick 'hyprctl dispatch workspace {}' ", index);
+		print!(":onclick 'hyprctl dispatch workspace {index}' ");
+		print!(":onrightclick 'hyprctl dispatch workspace {index}' ");
 
 		let class = match index {
 			_ if index == *focused => "focused",
-			_ if workspaces.contains(&index) => "active",
+			i if workspaces.contains(&i) => "active",
 			_ => "inactive",
 		};
 
@@ -34,21 +30,23 @@ pub fn print_workspaces(
 			workspaces.remove(&index);
 		}
 
-		print!(":class '{}' '{}')", class, icon);
+		print!(":class '{class}' '{icon}')");
 	}
 
 	for &index in workspaces.iter() {
-		print!(" (button :tooltip 'Workspace {}'", index);
+		let index = index as u8 + 1;
+
+		print!(" (button :tooltip 'Workspace {index}'");
 
 		if index == *focused {
-			print!(":class 'focused' '{}')", OTHER_ICON);
+			print!(":class 'focused' '{OTHER_ICON}')");
 			continue;
 		}
 
-		print!(":onclick 'hyprctl dispatch workspace {}' ", index);
-		print!(":onrightclick 'hyprctl dispatch workspace {}' ", index);
+		print!(":onclick 'hyprctl dispatch workspace {index}' ");
+		print!(":onrightclick 'hyprctl dispatch workspace {index}' ");
 
-		print!(":class 'active' '{}')", OTHER_ICON);
+		print!(":class 'active' '{OTHER_ICON}')");
 	}
 
 	println!("))");
@@ -56,19 +54,19 @@ pub fn print_workspaces(
 
 pub fn main() {
 	let sock_dir = match std::env::var("HYPRLAND_INSTANCE_SIGNATURE") {
-		Ok(path) => format!("/tmp/hypr/{}/", path),
+		Ok(sig) => PathBuf::from(format!("/tmp/hypr/{sig}/")),
 		Err(msg) => panic!("{}", msg),
 	};
 
-	let (mut workspaces, mut focused_workspace) = init(&sock_dir);
+	let (mut workspaces, mut focused_workspace) = init(&sock_dir).unwrap();
+
+	let stream_path = format!("{}.socket2.sock", sock_dir.display());
+	println!("; Connecting to: {stream_path}...");
+
+	let stream = UnixStream::connect(stream_path).unwrap();
+	let mut reader = BufReader::new(stream);
 
 	let exec_path: PathBuf = std::env::current_exe().unwrap();
-
-	println!("; Connecting to: {}.socket2.sock...", sock_dir);
-
-	let stream = UnixStream::connect(sock_dir + ".socket2.sock").unwrap();
-	stream.shutdown(std::net::Shutdown::Write).unwrap();
-	let mut reader = BufReader::new(stream);
 
 	println!("; Successed!");
 
@@ -84,62 +82,56 @@ pub fn main() {
 	}
 }
 
-pub fn init(sock_dir: &String) -> (BTreeSet<u8>, u8) {
+pub fn init(sock_dir: &PathBuf) -> Option<(BTreeSet<u8>, u8)> {
 	let mut workspaces: BTreeSet<u8> = BTreeSet::new();
 
-	println!("; Connecting to: {}.socket.sock...", sock_dir);
-	let mut stream = UnixStream::connect(sock_dir.clone() + ".socket.sock")
-		.expect("; Error: failed to connect to server.");
+	let stream_path = format!("{}.socket.sock", sock_dir.display());
 
-	stream.write(b"j/activeworkspace").unwrap();
-
-	let mut output = String::new();
-	stream.read_to_string(&mut output).unwrap();
-
-	let focused = json::parse(&output).unwrap()["id"].as_u8().unwrap();
-	stream.shutdown(std::net::Shutdown::Both).unwrap();
-
-	let mut stream =
-		UnixStream::connect(sock_dir.clone() + ".socket.sock").unwrap();
-
-	stream.write(b"j/workspaces").unwrap();
+	println!("; Connecting to: {stream_path}...");
+	let mut stream = UnixStream::connect(&stream_path).ok()?;
 
 	let mut output = String::new();
-	stream.read_to_string(&mut output).unwrap();
+	stream.write(b"j/activeworkspace").ok()?;
+	stream.read_to_string(&mut output).ok()?;
 
-	for workspace in json::parse(&output).unwrap().members() {
-		let id = workspace["id"].as_u8().unwrap();
+	let focused = json::parse(&output).ok()?["id"].as_u8()?;
+	stream.shutdown(std::net::Shutdown::Both).ok()?;
+
+	let mut stream = UnixStream::connect(stream_path).ok()?;
+
+	output.clear();
+	stream.write(b"j/workspaces").ok()?;
+	stream.read_to_string(&mut output).ok()?;
+
+	for workspace in json::parse(&output).ok()?.members() {
+		let id = workspace["id"].as_u8()?;
 		workspaces.insert(id);
 	}
 
-	return (workspaces, focused);
+	return Some((workspaces, focused));
 }
 
-pub fn update(
-	buf: String,
-	workspaces: &mut BTreeSet<u8>,
-	focused: &mut u8,
-) -> bool {
+pub fn update(buf: String, workspaces: &mut BTreeSet<u8>, focused: &mut u8) -> bool {
 	let mut changed = false;
+
+	fn get_number(number: &str) -> u8 {
+		return number.parse::<u8>().unwrap();
+	}
 
 	for line in buf.lines() {
 		if line.starts_with("workspace>>") {
-			let forcused_workspace: String = line.chars().skip(11).collect();
-			*focused = forcused_workspace.parse::<u8>().unwrap();
+			*focused = get_number(&line[11 ..]);
 
 			changed = true;
 		} else if line.starts_with("createworkspace>>") {
-			let created_workspace: String = line.chars().skip(16).collect();
-			workspaces.insert(created_workspace.parse::<u8>().unwrap());
+			workspaces.insert(get_number(&line[16 ..]));
 
 			changed = true;
 		} else if line.starts_with("destroyworkspace>>") {
-			let destroyed_workspace: String = line.chars().skip(17).collect();
-			let u8_destroyed = destroyed_workspace.parse::<u8>().unwrap();
+			let destroyed = get_number(&line[17 ..]);
 
-			if workspaces.contains(&u8_destroyed) {
-				workspaces.remove(&u8_destroyed);
-
+			if workspaces.contains(&destroyed) {
+				workspaces.remove(&destroyed);
 				changed = true;
 			}
 		}
