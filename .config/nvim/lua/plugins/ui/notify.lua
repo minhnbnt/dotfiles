4daf8c0d508db070ebc713d9dac9dfc99bd9b97a
minhnbnt <minhnbnt@gmail.com>
2023-11-04 11:06:01 +0700

    feat: use wrapped-compact and disable animation for nvim-notify

--- .config/nvim/lua/plugins/ui/notify.lua
+++ .config/nvim/lua/plugins/ui/notify.lua
@@ -1,12 +1,9 @@
 require("notify").setup({
 	background_colour = "#000000",
-	render = "compact",
-	stages = "slide",
+	render = "wrapped-compact",
+	stages = "static",
 	top_down = false,
 
-	max_height = function()
-		return math.floor(vim.o.lines * 0.75)
-	end,
 	max_width = function()
 		return math.floor(vim.o.columns * 0.75)
 	end,
