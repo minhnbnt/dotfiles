3e1e9db0c92daa014f01ac5a3136d9310d66ac0b
minhnbnt <minhnbnt@gmail.com>
2023-11-10 12:05:29 +0700

    refactor: just small changes for noice

--- .config/nvim/lua/plugins/ui/noice.lua
+++ .config/nvim/lua/plugins/ui/noice.lua
@@ -7,17 +7,13 @@ require("noice").setup({
 		},
 	},
 	lsp = {
-		progress = {
-			enabled = false,
-		},
+		progress = { enabled = false },
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true,
 		},
-		signature = {
-			enabled = false,
-		},
+		signature = { enabled = false },
 	},
 	presets = {
 		-- you can enable a preset by setting it to true, or a table that will override the preset config
@@ -26,7 +22,7 @@ require("noice").setup({
 		command_palette = true, -- position the cmdline and popupmenu together
 		long_message_to_split = true, -- long messages will be sent to a split
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
+		lsp_doc_border = true, -- add a border to hover docs and signature help
 	},
 	views = {
 		cmdline_popup = {
