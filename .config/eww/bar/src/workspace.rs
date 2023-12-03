381a13e5fa00fe6ddbbf600bdca505ac4a2b2fb3
minhnbnt <minhnbnt@gmail.com>
2023-12-03 21:02:44 +0700

    fix: some small changes and error handling

--- .config/eww/bar/src/workspace.rs
+++ .config/eww/bar/src/workspace.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeSet;
-use std::env::{args, current_exe, var};
-use std::io::{BufReader, Read, Write};
+use std::env;
+use std::io::{self, BufReader, Read, Write};
 use std::os::unix::net::UnixStream;
 use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
@@ -9,53 +9,55 @@ pub fn print_workspaces(exec_path: &Path, workspaces: &BTreeSet<u8>, focused: u8
 	const OTHER_ICON: &str = "";
 	const ICONS: [&str; 7] = ["", "", "", "", "", "", ""];
 
-	let mut workspaces = workspaces.clone();
-
 	print!("(eventbox :onscroll '{} {{}}' ", exec_path.display());
 	print!("(box :class 'works' :orientation 'v' :space-evenly false");
 
-	for (index, icon) in ICONS.iter().enumerate() {
-		let index = index as u8 + 1;
-
+	#[inline]
+	fn print_button(index: u8, class: &str, icon: &str) {
 		print!(" (button :tooltip 'Workspace {}' ", index);
 
 		print!(":onclick 'hyprctl dispatch workspace {}' ", index);
 		print!(":onrightclick 'hyprctl dispatch workspace {}' ", index);
-
-		let class: &'static str = match index {
-			_ if index == focused => "active",
-			i if workspaces.contains(&i) => "visible",
-			_ => "inactive",
-		};
-
-		if workspaces.contains(&index) {
-			workspaces.remove(&index);
-		}
 
 		print!(":class '{}' '{}')", class, icon);
 	}
 
-	for &index in workspaces.iter() {
-		print!(" (button :tooltip 'Workspace {}' ", index);
+	let mut workspaces = workspaces.clone();
 
-		if index == focused {
-			print!(":class 'active' '{}')", OTHER_ICON);
-			continue;
+	for (index, icon) in ICONS.iter().enumerate() {
+		let index = index as u8 + 1;
+
+		let mut class = "inactive";
+
+		if workspaces.contains(&index) {
+			workspaces.remove(&index);
+			class = "visible";
 		}
 
-		print!(":onclick 'hyprctl dispatch workspace {}' ", index);
-		print!(":onrightclick 'hyprctl dispatch workspace {}' ", index);
+		if index == focused {
+			class = "active";
+		}
 
-		print!(":class 'visible' '{}')", OTHER_ICON);
+		print_button(index, class, icon);
+	}
+
+	for index in workspaces {
+		let mut class = "visible";
+
+		if index == focused {
+			class = "active";
+		}
+
+		print_button(index, class, OTHER_ICON);
 	}
 
 	println!("))");
 }
 
-pub fn main() -> std::io::Result<()> {
+pub fn main() -> io::Result<()> {
 	arg_handle();
 
-	let sock_dir = match var("HYPRLAND_INSTANCE_SIGNATURE") {
+	let sock_dir = match env::var("HYPRLAND_INSTANCE_SIGNATURE") {
 		Ok(sig) => PathBuf::from(format!("/tmp/hypr/{}/", sig)),
 		Err(msg) => panic!("{}", msg),
 	};
@@ -68,35 +70,39 @@ pub fn main() -> std::io::Result<()> {
 	let stream = UnixStream::connect(stream_path)?;
 	let mut reader = BufReader::new(stream);
 
-	let exec_path: PathBuf = current_exe()?;
+	let exec_path: PathBuf = env::current_exe()?;
 
-	println!("; Successed!");
+	println!("; Succeed!");
 
 	print_workspaces(&exec_path, &workspaces, focused_workspace);
 
 	loop {
-		let mut bytes = [0; 1024];
-		let _ = reader.read(&mut bytes)?;
+		let mut bytes = vec![0; 1024];
 
-		let buffer = std::str::from_utf8(&bytes).unwrap();
+		match reader.read(&mut bytes) {
+			Ok(0) | Err(_) => continue,
+			_ => (),
+		};
 
-		if update(buffer, &mut workspaces, &mut focused_workspace) {
+		let buffer = String::from_utf8_lossy(&bytes);
+
+		if update(&buffer, &mut workspaces, &mut focused_workspace) {
 			print_workspaces(&exec_path, &workspaces, focused_workspace);
 		}
 	}
 }
 
 pub fn init(sock_dir: &Path) -> Option<(BTreeSet<u8>, u8)> {
-	let mut workspaces: BTreeSet<u8> = BTreeSet::new();
-
 	fn get_number(buf: &str) -> u8 {
 		let buf = buf[13 .. 15].trim_end();
 		buf.parse::<u8>().unwrap()
 	}
 
+	let mut workspaces: BTreeSet<u8> = BTreeSet::new();
+
 	let stream_path = format!("{}.socket.sock", sock_dir.display());
 
-	println!("; Connecting to: {stream_path}...");
+	println!("; Connecting to: {}...", stream_path);
 	let mut stream = UnixStream::connect(&stream_path).ok()?;
 
 	let mut output = String::new();
@@ -104,9 +110,8 @@ pub fn init(sock_dir: &Path) -> Option<(BTreeSet<u8>, u8)> {
 	stream.read_to_string(&mut output).ok()?;
 
 	let focused = get_number(&output);
-	stream.shutdown(std::net::Shutdown::Both).ok()?;
 
-	let mut stream = UnixStream::connect(stream_path).ok()?;
+	stream = UnixStream::connect(stream_path).ok()?;
 
 	output.clear();
 	stream.write_all(b"workspaces").ok()?;
@@ -158,13 +163,15 @@ pub fn update(buffer: &str, workspaces: &mut BTreeSet<u8>, focused: &mut u8) -> 
 }
 
 pub fn arg_handle() {
-	let args: Vec<String> = args().collect();
+	let args: Vec<String> = env::args().collect();
 
 	if args.len() == 1 {
 		return;
 	}
 
-	assert!(args.len() == 2, "Too many arguments.");
+	if args.len() != 2 {
+		panic!("Invalid argument.");
+	}
 
 	let mut command = Command::new("hyprctl");
 	let mut args_command = vec!["dispatch", "workspace"];
@@ -176,7 +183,9 @@ pub fn arg_handle() {
 		_ => panic!("Invalid argument."),
 	};
 
-	command.args(args_command).spawn().unwrap();
+	if command.args(args_command).spawn().is_err() {
+		panic!("Failed to execute command.");
+	}
 
 	exit(0);
 }
