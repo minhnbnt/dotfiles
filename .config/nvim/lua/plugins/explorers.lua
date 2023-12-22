907a0fe0ba5d1c0f70611182c065127c1158b942
minhnbnt <minhnbnt@gmail.com>
2023-12-22 17:34:08 +0700

    feat: use telescope file_browser when open a directory

--- .config/nvim/lua/plugins/explorers.lua
+++ .config/nvim/lua/plugins/explorers.lua
@@ -1,6 +1,52 @@
 local Plug = require("core.functions").plugin
 
+local function open_file_browser()
+	local current_dir = vim.fn.expand("%:p")
+	local stat = vim.loop.fs_stat(current_dir)
+
+	if stat and stat.type == "directory" then
+		vim.api.nvim_buf_delete(0, { force = true })
+		vim.cmd(":Telescope file_browser hidden=true grouped=true")
+	end
+end
+
 return {
+
+	Plug("nvim-tree/nvim-tree.lua", {
+
+		version = "*",
+		lazy = false,
+
+		opts = {
+			sort_by = "case_sensitive",
+			update_focused_file = {
+				enable = true,
+				update_cwd = true,
+			},
+			view = {
+				width = 30,
+				side = "left",
+			},
+			hijack_directories = {
+				enable = false,
+			},
+			respect_buf_cwd = true,
+			update_cwd = true,
+			hijack_cursor = false,
+			sync_root_with_cwd = true,
+		},
+
+		init = function()
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+
+			vim.api.nvim_create_autocmd({ "QuitPre" }, {
+				callback = function()
+					vim.cmd("NvimTreeClose")
+				end,
+			})
+		end,
+	}),
 
 	Plug("nvim-telescope/telescope.nvim", {
 
@@ -26,6 +72,10 @@ return {
 		},
 
 		init = function()
+			vim.api.nvim_create_autocmd("VimEnter", {
+				callback = open_file_browser,
+			})
+
 			require("telescope").load_extension("file_browser", "fzy_native", "projects")
 		end,
 
@@ -34,38 +84,5 @@ return {
 				-- borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
 			},
 		},
-	}),
-
-	Plug("nvim-tree/nvim-tree.lua", {
-
-		version = "*",
-		lazy = false,
-
-		opts = {
-			sort_by = "case_sensitive",
-			update_focused_file = {
-				enable = true,
-				update_cwd = true,
-			},
-			view = {
-				width = 30,
-				side = "left",
-			},
-			respect_buf_cwd = true,
-			update_cwd = true,
-			hijack_cursor = false,
-			sync_root_with_cwd = true,
-		},
-
-		init = function()
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
-
-			vim.api.nvim_create_autocmd({ "QuitPre" }, {
-				callback = function()
-					vim.cmd("NvimTreeClose")
-				end,
-			})
-		end,
 	}),
 }
