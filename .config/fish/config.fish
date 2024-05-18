56f51136bbc0512705a5955e1b291bbc34e484a1
minhnbnt <minhnbnt@gmail.com>
2024-05-18 08:04:47 +0700

    fix: switched to fastfetch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     clear
-    neofetch --config /home/minhnbnt/.config/neofetch/configfish.conf | sed '/^$/d'
+	fastfetch
 end
 
 set fish_greeting
