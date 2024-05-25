ca07e50430fa07e4b77170bbfda285a6e68d5bfd
minhnbnt <minhnbnt@gmail.com>
2024-05-25 19:28:16 +0700

    fix: migrate to dwm 6.5

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -80,9 +80,10 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf",
-                                  col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1,
+                                  "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
 /* for volume control */
 static const char *upvol[]   = { "amixer", "set", "Master", "2+", NULL };
