ecbf8b202eb357084e4c7bf807fcea6b405034b8
minhnbnt <minhnbnt@gmail.com>
2024-06-01 07:59:10 +0700

    feat: add some zsh plugins

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,20 +1,12 @@
-ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+ZOXIDE_CMD_OVERRIDE=cd
 
-if [ ! -d $ZINIT_HOME ]; then
-	mkdir -p "$(dirname $ZINIT_HOME)"
-fi
-
-if [ ! -d $ZINIT_HOME/.git ]; then
-	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-fi
-
-source "${ZINIT_HOME}/zinit.zsh"
-
-zinit light marlonrichert/zsh-autocomplete
 zinit light hlissner/zsh-autopair
+zinit light marlonrichert/zsh-autocomplete
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-syntax-highlighting
 
-eval "$(atuin init zsh)"
-eval "$(thefuck --alias)"
-eval "$(zoxide init zsh --cmd cd)"
+zinit snippet OMZP::archlinux
+zinit snippet OMZP::git
+zinit snippet OMZP::sudo
+zinit snippet OMZP::starship
+zinit snippet OMZP::zoxide
--- .zshrc
+++ .zshrc
@@ -7,6 +7,18 @@ if [[ $- != *i* ]]; then
 	return
 fi
 
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+if [ ! -d $ZINIT_HOME ]; then
+	mkdir -p "$(dirname $ZINIT_HOME)"
+fi
+
+if [ ! -d $ZINIT_HOME/.git ]; then
+	git clone git@github.com:zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+source "${ZINIT_HOME}/zinit.zsh"
+
 fastfetch
 
 for conf in "$HOME/.config/zsh/"*.zsh; do
@@ -14,3 +26,8 @@ for conf in "$HOME/.config/zsh/"*.zsh; do
 done
 
 unset conf
+
+autoload -Uz compinit
+compinit
+
+zinit cdreplay -q
