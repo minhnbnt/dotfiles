82a75e1d9871b191c30957a579b4dc7a3a33bd83
minhnbnt <minhnbnt@gmail.com>
2024-11-22 10:30:18 +0700

    fix: use new way to bootstrap lazy.nvim

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,30 +1,29 @@
-local function Lazy_init()
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 
-	-- stylua: ignore
-	if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		vim.fn.system({	"git", "clone", "--branch=stable", "--filter=blob:none",
-		                "git@github.com:folke/lazy.nvim.git", lazypath })
-	end
-
-	vim.opt.rtp:prepend(lazypath)
-
-	require("lazy").setup({
-
-		spec = { import = "plugins" },
-
-		concurrency = 10,
-
-		git = {
-			timeout = 500,
-			url_format = "git@github.com:%s.git",
-		},
-
-		ui = { border = "rounded" },
-
-		change_detection = { notify = false },
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--branch=stable",
+		"--filter=blob:none",
+		lazyrepo,
+		lazypath,
 	})
+
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
+
+vim.opt.rtp:prepend(lazypath)
 
 if vim.loader then
 	vim.loader.enable()
@@ -34,4 +33,17 @@ require("core.options")
 require("core.autocmd")
 require("core.keymaps")
 
-Lazy_init()
+require("lazy").setup({
+
+	spec = { import = "plugins" },
+
+	concurrency = 10,
+
+	git = {
+		timeout = 500,
+	},
+
+	ui = { border = "rounded" },
+
+	change_detection = { notify = false },
+})
