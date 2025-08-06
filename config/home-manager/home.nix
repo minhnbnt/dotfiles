ff88212416fe8709334a5f331711a728459f58fe
minhnbnt <minhnbnt@gmail.com>
2025-08-06 17:04:58 +0700

    feat(home-manager): added some packages

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -27,11 +27,14 @@
   programs.zed-editor.enable = true;
   programs.home-manager.enable = true;
 
+  programs.java.enable = true;
+
   home.packages = with pkgs; [
 
     brave
     librewolf
     qutebrowser
+    xfce.thunar
 
     gimp3
     vlc
@@ -39,12 +42,18 @@
     jetbrains.idea-community-bin
     jetbrains.pycharm-community-bin
 
+    clang
     pnpm
+    (buildFHSEnv {
+      name = "pixi";
+      runScript = "pixi";
+      targetPkgs = pkgs: [ pkgs.pixi ];
+    })
     nodePackages_latest.nodejs
 
-    clang
     gitleaks
     hurl
+    jq
     kubectl
     lazydocker
     minikube
